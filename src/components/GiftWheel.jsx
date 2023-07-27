d6e47a734a476cfa4516cf53b976214a00fe57f1
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-28 00:33:09 +0300

    Created Gift Wheel component.

--- src/components/GiftWheel.jsx
+++ src/components/GiftWheel.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import SpinAndWin from "react-spin-game";
 import "react-spin-game/dist/index.css";
 
@@ -9,9 +8,19 @@ const GiftWheel = () => {
     ["test3", "#808080"],
     ["test4", "blue"],
     ["test5", "gray"],
-    ["test6", "blue"],
+    ["test6", "green"],
   ];
-  return <SpinAndWin data={freeSpinGifts}/>;
+  return (
+    <div className="w-[600px] h-[550px] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] mx-auto rounded-lg mb-4 flex flex-col gap-y-2 items-center justify-center">
+      <SpinAndWin
+        data={freeSpinGifts}
+        result="test5"
+        fontSize={23}
+        horizantalText={true}
+      />
+      <h1 className="text-xl text-blue-800 font-bold">Click Spin Button</h1>
+    </div>
+  );
 };
 
 export default GiftWheel;
