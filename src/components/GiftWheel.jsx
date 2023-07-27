2a2684b77aa5f683f15ffe3d60ac493159bcf8ef
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-28 01:11:40 +0300

    Updated GiftWheel component.

--- src/components/GiftWheel.jsx
+++ src/components/GiftWheel.jsx
@@ -1,24 +1,35 @@
+import React, { useState, useEffect } from "react";
+//Components
+import LoaderSpinner from "./LoaderSpinner";
+// Spin Package
 import SpinAndWin from "react-spin-game";
 import "react-spin-game/dist/index.css";
-
+// Config
+import { prizes } from "../utils/prizes.config.json";
+//Utils
+import { setList } from "../utils/spin-functions";
 const GiftWheel = () => {
-  const freeSpinGifts = [
-    ["test1", "red"],
-    ["test2", "black"],
-    ["test3", "#808080"],
-    ["test4", "blue"],
-    ["test5", "gray"],
-    ["test6", "green"],
-  ];
+  const [spinList, setSpinList] = useState([]);
+  useEffect(() => {
+    const prizeList = setList(prizes);
+    setSpinList(prizeList);
+  }, []);
+
   return (
     <div className="w-[600px] h-[550px] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] mx-auto rounded-lg mb-4 flex flex-col gap-y-2 items-center justify-center">
-      <SpinAndWin
-        data={freeSpinGifts}
-        result="test5"
-        fontSize={23}
-        horizantalText={true}
-      />
-      <h1 className="text-xl text-blue-800 font-bold">Click Spin Button</h1>
+      {spinList.length !== 0 ? (
+        <>
+          <SpinAndWin
+            data={spinList}
+            result="test5"
+            fontSize={23}
+            horizantalText={false}
+          />
+          <h1 className="text-xl text-blue-800 font-bold">Click Spin Button</h1>
+        </>
+      ) : (
+        <LoaderSpinner />
+      )}
     </div>
   );
 };
