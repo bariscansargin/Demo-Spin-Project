8b3bd66be32f47b61ca2d9d864dbb4d28b7185e7
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-28 00:32:51 +0300

    Created Header Component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header>
-      <h1></h1>
+    <header className="flex items-center justify-center p-4 mb-4 gap-x-12">
+      <h1 className="text-slate-700 font-extrabold text-2xl tracking-widest">
+        GIFT WHEEL
+      </h1>
+      <img src="../src/assets/logo.png" alt="logo" className="w-32 h-18" />
     </header>
   );
 };
