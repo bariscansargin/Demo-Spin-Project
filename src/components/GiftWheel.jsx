055ac432af34356b5e5c57f135b37614bb0cb527
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-28 12:27:15 +0300

    Update GiftWheel.jsx

--- src/components/GiftWheel.jsx
+++ src/components/GiftWheel.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 //Components
 import LoaderSpinner from "./LoaderSpinner";
 // Spin Package
@@ -10,22 +10,40 @@ import { prizes } from "../utils/prizes.config.json";
 import { setList } from "../utils/spin-functions";
 const GiftWheel = () => {
   const [spinList, setSpinList] = useState([]);
+  const [isSpinning, setIsSpinning] = useState(false);
+  const buttonRef = useRef(null);
+
   useEffect(() => {
     const prizeList = setList(prizes);
-    setSpinList(prizeList);
+    setTimeout(() => {
+      setSpinList(prizeList);
+    }, 1000);
   }, []);
-
+  const spinHandler = () => {};
   return (
     <div className="w-[600px] h-[550px] shadow-[0_20px_50px_rgba(8,_112,_184,_0.7)] mx-auto rounded-lg mb-4 flex flex-col gap-y-2 items-center justify-center">
       {spinList.length !== 0 ? (
         <>
           <SpinAndWin
             data={spinList}
-            result="test5"
+            result="250 SMS"
             fontSize={23}
             horizantalText={false}
+            ref={buttonRef}
+            hideButton={true}
+            fontFamily="fantasy"
           />
-          <h1 className="text-xl text-blue-800 font-bold">Click Spin Button</h1>
+          {!isSpinning && (
+            <button
+              className=" bg-blue-600 hover:bg-blue-400 hover:text-black  p-2 mt-2 rounded-lg text-white shadow-lg text-md font-bold transition-all duration-300 ease-out"
+              onClick={() => {
+                spinHandler();
+                buttonRef.current.handleSpin();
+              }}
+            >
+              Click for Spin
+            </button>
+          )}
         </>
       ) : (
         <LoaderSpinner />
