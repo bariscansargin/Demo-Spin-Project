ce9a2a90bc2ec373df87d55ddd9b51a0640b072b
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-28 15:10:16 +0300

    Created next spin date function.

---
+++ src/utils/date-functions.js
@@ -0,0 +1,11 @@
+export const calculateNextSpin = () => {
+  const today = new Date();
+  const fiveDaysAhead = new Date(today.getTime() + 5 * 24 * 60 * 60 * 1000);
+
+  const year = fiveDaysAhead.getFullYear();
+  const month = String(fiveDaysAhead.getMonth() + 1).padStart(2, "0");
+  const day = String(fiveDaysAhead.getDate()).padStart(2, "0");
+
+  const formattedDate = `${day}-${month}-${year}`;
+  return formattedDate;
+};
