08579498f7c5194049e8716b88dca1594f3d8420
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-29 17:32:37 +0300

    Update date-functions.js

--- src/utils/date-functions.js
+++ src/utils/date-functions.js
@@ -2,7 +2,7 @@ export const calculateNextSpin = () => {
   const today = new Date();
   const fiveDaysAhead = new Date(today.getTime() + 5 * 24 * 60 * 60 * 1000);
 
-  const year = fiveDaysAhead.getFullYear();
+  const year = String(fiveDaysAhead.getFullYear());
   const month = String(fiveDaysAhead.getMonth() + 1).padStart(2, "0");
   const day = String(fiveDaysAhead.getDate()).padStart(2, "0");
 
