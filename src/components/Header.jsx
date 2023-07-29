888d2fa42f1c5cda37218b92bb69ac92f64d25f1
Barışcan Sargın <bariscansargin4@gmail.com>
2023-07-29 19:03:43 +0300

    Update styling.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="flex flex-col items-center justify-center p-4  gap-y-8 mb-36">
+    <header className="flex flex-col items-center justify-center p-4  gap-y-8 mb-36 min-[382px]:pt-0 sm:mb-4 min-[380px]:mb-4">
       <h1 className="text-slate-600 font-extrabold text-2xl tracking-widest">
         GIFT WHEEL
       </h1>
 
-      <img src="../src/assets/logo.png" alt="logo" className="w-32 h-18" />
+      <img src="../src/assets/logo.png" alt="logo" className="w-32 h-18 sm:w-24 h-16 lg:w-32 h-18" />
     </header>
   );
 };
